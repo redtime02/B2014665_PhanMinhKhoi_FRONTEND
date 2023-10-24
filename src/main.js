36ba47bb552ac85f5f9f2a42231ecbda5f3db0c8
Phan Minh Khoi B2014665 <phanthanhbinh089@gmail.com>
2023-10-24 11:13:49 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+// import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import router from "./router";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
